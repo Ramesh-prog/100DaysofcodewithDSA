bd156ef181c8a0f5c78307796ffbd9dc1438c1eb
Ramesh <rameshmohapatra2078@gmail.com>
2021-11-30 19:16:50 +0530

    commit

---
+++ Day5/longest substring without repeating char.java
@@ -0,0 +1,21 @@
+/**
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+*/
+
+class Solution 
+{
+    public int lengthOfLongestSubstring(String s) 
+    { 
+        int result = 0;
+        int[] cache = new int[256];
+        for (int i = 0, j = 0; i < s.length(); i++) 
+        {
+            j = (cache[s.charAt(i)] > 0) ? Math.max(j, cache[s.charAt(i)]) : j;
+            cache[s.charAt(i)] = i + 1;
+            result = Math.max(result, i - j + 1);
+        }
+        return result;
+     }
+}
